0ce44ffe6ec4bcb918ae67adc92620edd486d5cc
AntoineRst <antoine.resta@me.com>
2021-10-15 11:58:02 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -4,38 +4,44 @@ import sys   #nécéssaire pour afficher les erreurs système
 import psutil #nécéssaire pour trouver les interfaces réseau de la machine
 import ipaddress #nécéssaire pour trouver le réseau local
 import os.path  #nééssaire pour tester l'existence d'un fichier
-import colorama
 from datetime import datetime
-colorama.init()
+
+def getInfo(hotesListe,nouv_fich,aff_err,port,ch_fich):
+    for i in hotesListe:                               #On parcourt la liste des @ des hôtes entrés par l'utilisateur
+        try :
+            client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.settimeout(0.05)              #Attend 50ms avant de passer à l'essai suivant
+        except :
+            print (Colors.NEGATIVE+"Création de la socket impossible pour "+str(i)+Colors.END)
+        try :
+            client_socket.connect((str(i), port))                                                        #Essaye de se connecter à l'hôte sur le port spécifié
+            message_recu = client_socket.recv(1024).decode() 
+            if ch_fich.upper()=="OUI":
+                nouv_fich.write("["+str(i)+"] :"+message_recu+"\n")                                       #Decode le message reçu de l'hôte
+            print (Colors.YELLOW+"["+Colors.END+str(i)+Colors.YELLOW+"]"+Colors.END+": "+message_recu)   #Affiche le message/les infos sous la forme [@IPmachine] : OS,Utilisateurs
+            client_socket.close()                                                                   #Ferme la connexion
+        except :
+            if aff_err.upper()=="OUI": 
+                print(Colors.NEGATIVE+"Connexion à "+str(i)+" impossible"+Colors.END)                        #Erreur "simple"
+                if ch_fich.upper()=="OUI":
+                    nouv_fich.write("Connexion à "+str(i)+" impossible\n")
+                print(sys.exc_info())
 
 # ------------------ Classe couleurs
 # SGR color constants
 # rene-d 2018 https://gist.github.com/rene-d/9e584a7dd2935d0f461904b9f2950007
 class Colors:
     """ ANSI color codes """
-    BLACK = "\033[0;30m"
     RED = "\033[0;31m"
     GREEN = "\033[0;32m"
-    BROWN = "\033[0;33m"
-    BLUE = "\033[0;34m"
     PURPLE = "\033[0;35m"
     CYAN = "\033[0;36m"
-    LIGHT_GRAY = "\033[0;37m"
     DARK_GRAY = "\033[1;30m"
-    LIGHT_RED = "\033[1;31m"
-    LIGHT_GREEN = "\033[1;32m"
     YELLOW = "\033[1;33m"
     LIGHT_BLUE = "\033[1;34m"
-    LIGHT_PURPLE = "\033[1;35m"
-    LIGHT_CYAN = "\033[1;36m"
-    LIGHT_WHITE = "\033[1;37m"
     BOLD = "\033[1m"
-    FAINT = "\033[2m"
-    ITALIC = "\033[3m"
-    UNDERLINE = "\033[4m"
     BLINK = "\033[5m"
     NEGATIVE = "\033[7m"
-    CROSSED = "\033[9m"
     END = "\033[0m"
 
 #  ------------------ Création liste des interfaces réseau de la machine
@@ -46,35 +52,21 @@ for i in int_ps.keys():         #ajoute à la liste int_list chaque nom d'interf
     
 # ------------------ Fonction SCAN d'@IP rentrées manuellement
 def ip_liste():
-    colorama.init()  
+    aff_err="OUI" 
     print(Colors.RED+'/!\\'+Colors.YELLOW+' Entrez les adresses IP des Machines séparées par ";" : \n'+Colors.END)
     hotes=input(Colors.YELLOW+">> "+Colors.END)
     port=14528                                       #port utilisé pour la connexion
     hotesListe = hotes.split(';')                    #transforme la chaîne de caractère entrée en liste
-    for i in hotesListe:                               #On parcourt la liste des @ des hôtes entrés par l'utilisateur
-        try :
-            client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.settimeout(0.05)              #Attend 50ms avant de passer à l'essai suivant
-        except :
-            print (Colors.NEGATIVE+"Création de la socket impossible pour "+i+Colors.END)
-        try :
-            client_socket.connect((i, port))                                                        #Essaye de se connecter à l'hôte sur le port spécifié
-            message_recu = client_socket.recv(1024).decode() 
-            if ch_fich.upper()=="OUI":
-                nouv_fich.write("["+i+"] :"+message_recu+"\n")                                       #Decode le message reçu de l'hôte
-            print (Colors.YELLOW+"["+Colors.END+i+Colors.YELLOW+"]"+Colors.END+": "+message_recu)   #Affiche le message/les infos sous la forme [@IPmachine] : OS,Utilisateurs
-            client_socket.close()                                                                   #Ferme la connexion
-        except :
-            print(Colors.NEGATIVE+"Connexion à "+i+" impossible"+Colors.END)                        #Erreur "simple"
-            nouv_fich.write("Connexion à "+str(i)+" impossible\n")
-            print(sys.exc_info())                                                                   #Erreur système "précise"
+
+    #-----------  SCAN -------------
+    getInfo(hotesListe,nouv_fich,aff_err,port,ch_fich)
+
     if ch_fich.upper()=="OUI":
         nouv_fich.close()
 
 # ----------------- Fonction SCAN d'@IP du réseau local
 
 def ip_local():
-    colorama.init()
     aff_err="OUI"                       #Par défaut, variable aff_err en OUI pour afficher toutes les erreurs
     port=14528                           #port utilisé pour la connexion
     print(Colors.YELLOW+"---- Scan du réseau local ----\n"+Colors.RED+"/!\\"+Colors.YELLOW+"Ecrivez le nom de l'interface (sans @IP) a utiliser pour détecter le réseau local"+Colors.END)
@@ -93,30 +85,16 @@ def ip_local():
     listing_ip = list(reseau_hote.hosts()) #liste de toutes les machines du réseau
     print(Colors.GREEN+str(len(listing_ip))+" machines trouvées"+Colors.END)
     print(Colors.GREEN+"** Début du scan **"+Colors.END)
-    for i in listing_ip:   #Connexion à chaque machine (IP) trouvée auparavant
-        try :  
-            client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.settimeout(0.05)      #Attend 50ms avant de passer à l'essai suivant
-        except :
-            print (Colors.NEGATIVE+"Création de la socket impossible pour "+str(i)+Colors.END)
-        try :
-            client_socket.connect((str(i), port))                                               #Essaye de se connecter à l'hôte sur le port spécifié (converti en string)
-            message_recu = client_socket.recv(1024).decode() 
-            if ch_fich.upper()=="OUI":
-                nouv_fich.write("["+i+"] :"+message_recu+"\n")                                   #Decode le message reçu de l'hôte                                    
-            print (Colors.YELLOW+"["+Colors.END+str(i)+Colors.YELLOW+"]"+Colors.END+": "+message_recu)  #Affiche le message/les infos sous la forme [@IPmachine] : OS,Utilisateurs
-            client_socket.close()                                                               #Ferme la connexion
-        except :
-            if aff_err.upper()=="OUI": 
-                print(Colors.NEGATIVE+"Connexion à "+str(i)+" impossible"+Colors.END)             #Erreur "simple"                                                             
-                print(sys.exc_info())                                                           #Erreur système "précise" SI l'utilisation l'a choisi
+    
+    #-----------  SCAN -------------
+    getInfo(listing_ip,nouv_fich,aff_err,port,ch_fich)
+    
     if ch_fich.upper()=="OUI":
         nouv_fich.close()
 
 # ------------------ Fonction SCAN d'@IP depuis un fichier  
   
 def ip_fichier():                       #Par défaut, variable aff_err en OUI pour afficher toutes les erreurs
-    colorama.init()
     aff_err="OUI"   
     port=14528                           #port utilisé pour la connexion     
     print(Colors.YELLOW+"---- Scan a partir d'un fichier ----\nEntrez le nom du fichier (avec extension)"+Colors.END)
@@ -134,27 +112,12 @@ def ip_fichier():                       #Par défaut, variable aff_err en OUI po
     for i in range(len(listing_ip_fich)) :          #On parcours la liste
         if "\n" in listing_ip_fich[i] :             #Test si lors de la transformation, un retour à la ligne sous forme \n est apparu
             listing_ip_fich[i]=listing_ip_fich[i][0:-1]     #A ce moment on supprime ce retour à la ligne
-    for i in listing_ip_fich:   #Connexion à chaque machine (IP) trouvée dans le fichier
-        try :         #IDEM que les fonctions précédentes niveau connexion aux machines
-            client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.settimeout(0.05)
-        except :
-            print (Colors.NEGATIVE+"Création de la socket impossible pour "+str(i)+Colors.END)
-        try :
-            client_socket.connect((str(i), port))
-            message_recu = client_socket.recv(1024).decode()
-            if ch_fich.upper()=="OUI":
-                nouv_fich.write("["+i+"] :"+message_recu+"\n")
-            print (Colors.YELLOW+"["+Colors.END+str(i)+Colors.YELLOW+"]"+Colors.END+": "+message_recu) 
-            client_socket.close()
-        except :
-            if aff_err.upper()=="OUI": 
-                print(Colors.NEGATIVE+"Connexion à "+str(i)+" impossible"+Colors.END)
-                print(sys.exc_info())
-    f.close()
+
+    #----------- SCAN -------------
+    getInfo(listing_ip_fich,nouv_fich,aff_err,port,ch_fich)
+
     if ch_fich.upper()=="OUI":
         nouv_fich.close()
-
 
     
 fct_dict=[ip_liste,ip_local,ip_fichier,exit]            #Liste des fonctions séléctionnables dans le menu
@@ -166,7 +129,7 @@ while True :
 / _\_   _ ___    / _\ ___ __ _ _ __  
 \ \| | | / __|   \ \ / __/ _` | '_ \ 
 _\ \ |_| \__ \   _\ \ (_| (_| | | | |
-\__/\__, |___/___\__/\___\__,_|_| |_|v1.1
+\__/\__, |___/___\__/\___\__,_|_| |_|v1.2
     |___/ 
     """)
     print("--------------------------------------------"+Colors.END)
@@ -183,7 +146,9 @@ _\ \ |_| \__ \   _\ \ (_| (_| | | | |
             print(Colors.YELLOW+"-----------------------------")
             now = datetime.now()
             dt_string = now.strftime("%d_%m_%Y-%H_%M_%S")
-            nouv_fich=open("./logs/log-"+dt_string+".txt",'w')
+            nouv_fich=open("./logs/log-"+dt_string+".txt",'w+')
+        else:
+            nouv_fich=0
 
     if ch in range(1,5):            #test si l'utilisateur séléctionne une fonction existante
         fct_dict[ch-1]()            #execute la fonction choisie
